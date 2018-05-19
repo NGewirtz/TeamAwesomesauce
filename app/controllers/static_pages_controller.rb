35a5d100a73eb6495e17c2e069ba1b026ae77ef1
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-19 14:30:28 -0400

    Get http party to pull api data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
+require 'httparty'
+
 class StaticPagesController < ApplicationController
   def root
     auth = {:username => "7950b9f2b4f040a190896f92c9ed81c6", :password => ""}
-    @json = HTTParty.get("https://nestiolistings.com/api/v2/listings/residential/rentals/", :basic_auth => auth).body
+    @json = HTTParty.get("https://nestiolistings.com/api/v2/listings/residential/rentals/", :basic_auth => auth)
     puts @json
   end
 end
