c3908de7df200b221eb18d7e8750c83a13547735
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-19 12:38:09 -0400

    Create store and root reducer

--- frontend/nestiyoooo.jsx
+++ frontend/nestiyoooo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import configureStore from './store/store';
+import configureStore from './store/store';
 //import Root from './components/root';
 
 //Testing start
@@ -9,19 +9,19 @@ import ReactDOM from 'react-dom';
 //Testing end
 
 document.addEventListener('DOMContentLoaded', () => {
-  //window.$ = $;
+  window.$ = $;
   const root = document.getElementById('root');
-  // let store;
-  // if (window.currentUser) {
-  //   const preloadedState = { session: { currentUser: window.currentUser } };
-  //   store = configureStore(preloadedState);
-  //   window.getState = store.getState;
-  //   window.dispatch = store.dispatch;
-  //   delete window.currentUser;
-  // } else {
-  //   store = configureStore();
-  //   window.getState = store.getState;
-  //   window.dispatch = store.dispatch;
-  // }
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+  }
   ReactDOM.render(<h1>Welcome to NestiYoooo</h1>, root);
 });
