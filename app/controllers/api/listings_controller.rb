bdd2fcc095f66dc283c3dc671044133d898bbc44
Neil Gewirtz <ngewirtz86@gmail.com>
2018-05-19 18:38:43 -0400

    locations

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -4,7 +4,15 @@ class Api::ListingsController < ApplicationController
     bathrooms = params[:bathrooms]
     neighborhood = params[:neighborhood]
     square_footage = params[:square_footage]
-    render json: Listing.price(bedrooms, bathrooms, neighborhood, square_footage)
+    locations = Listing.where(neighborhood: neighborhood).pluck(:latitude, :longitude)
+    respond_to do |format|
+      format.json do
+        render json: {
+          price: Listing.price(bedrooms, bathrooms, neighborhood, square_footage),
+          locations: locations
+        }.to_json
+      end
+    end
   end
 
 end
