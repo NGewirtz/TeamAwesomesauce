0b275c6e59c8f6e6dd7202332cc0d66aab7a52fc
Robert O'Connor <robertgoconnor2@gmail.com>
2018-05-20 11:39:32 -0400

    Add neighborhood and bathroom filters to filter form

--- frontend/components/listings_filter_form.jsx
+++ frontend/components/listings_filter_form.jsx
@@ -12,6 +12,23 @@ class ListingsFilterForm extends React.Component {
     this.props.updateFilter("bedrooms", bedroomValue);
   }
 
+  createNeighborhoodOptions() {
+    let neighborhoods = ["West Village", "Hudson Heights", "Noho","Midtown",
+    "Upper West Side","Midtown West","Greenwich Village","Upper East Side",
+    "Jersey City","Riverdale","Union Square","DUMBO","Williamsburg",
+    "Fort Greene","Inwood","Tribeca","Financial District","Harlem",
+    "Battery Park City","Marble Hill","NoMad","Downtown Brooklyn","Bushwick",
+    "Gramercy","Flatiron","Midtown East","Nolita","Washington Heights",
+    "Kips Bay","Chelsea","Carroll Gardens","Cobble Hill","Meatpacking",
+    "East Williamsburg","Lower East Side","Edgewater Borough","Crown Heights",
+    "East Village","Brooklyn Heights", "Greenpoint","Forest Hills",
+    "East Flatbush","Bedford-Stuyvesant","Corona","Soho","Clinton Hill",
+    "Long Island City","Roosevelt Island","Hunts Point","Midwood","Georgetown"].sort();
+    return neighborhoods.map(neighborhood => (
+      <option value="{neighborhood}">{neighborhood}</option>
+    ));
+  }
+
   render() {
     return (
       <div className="filter-form-container">
@@ -19,11 +36,30 @@ class ListingsFilterForm extends React.Component {
           <div>
             <label htmlFor="bedrooms">Bedrooms</label>
             <select id="filter-bedroom-select" name="bedrooms" onChange={() => this.updateBedroomsFilter()}>
-              <option value="All">All</option>
+              <option value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
               <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+            <label htmlFor="bathrooms">Bathrooms</label>
+            <select name="bathrooms">
+              <option value=".5">1</option>
+              <option value=".5">1.5</option>
+              <option value=".5">2</option>
+              <option value=".5">2.5</option>
+              <option value=".5">3</option>
+              <option value=".5">3.5</option>
+              <option value=".5">4</option>
+              <option value=".5">4.5</option>
+              <option value=".5">5</option>
+              <option value=".5">5.5</option>
+            </select>
+            <label htmlFor="neighborhoods">Neighborhood</label>
+            <select name="neighborhoods">
+              {this.createNeighborhoodOptions()}
             </select>
           </div>
         </div>
