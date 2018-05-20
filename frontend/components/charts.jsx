ab54a948a25dc0f25c12a89e1e146d21487e39b5
Chao Fan <fanchao05@gmail.com>
2018-05-20 13:42:38 -0400

    updated styling

--- frontend/components/charts.jsx
+++ frontend/components/charts.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import DonutChart from './donut_chart';
 
 class Charts extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { ctx: null };
-  }
-
-  componentDidMount() {
-    const ctx = document
-      .querySelector('.chartjs-render-monitor')
-      .getContext('2d');
-
-    this.setState({ ctx });
-  }
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = { ctx: null };
+  // }
+  //
+  // componentDidMount() {
+  //   const ctx = document
+  //     .querySelector('.chartjs-render-monitor')
+  //     .getContext('2d');
+  //
+  //   this.setState({ ctx });
+  // }
 
   render() {
     const animationList = [
@@ -35,45 +35,39 @@ class Charts extends React.Component {
         Math.floor(Math.random() * Math.floor(animationList.length))
       ];
 
-    let gradient;
-
-    if (this.state.ctx) {
-      let ctx = this.state.ctx;
-      gradient = ctx.createLinearGradient(0, 0, 200, 0);
-      gradient.addColorStop(0, '#4facfe');
-      gradient.addColorStop(1, '#00f2fe');
-      ctx.fillStyle = gradient;
-      ctx.fillRect(10, 10, 200, 100);
-    }
-
-    let data = {
-      labels: ['price'],
-      datasets: [
-        {
-          data: [this.props.price],
-          backgroundColor: gradient,
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(120, 187, 232, 0.4)'
-        }
-      ]
-    };
-    let options = {
-      legend: {
-        display: false
-      }
-    };
+    // let gradient;
+    //
+    // if (this.state.ctx) {
+    //   let ctx = this.state.ctx;
+    //   gradient = ctx.createLinearGradient(0, 0, 200, 0);
+    //   gradient.addColorStop(0, '#4facfe');
+    //   gradient.addColorStop(1, '#00f2fe');
+    //   ctx.fillStyle = gradient;
+    //   ctx.fillRect(10, 10, 200, 100);
+    // }
+    //
+    // let data = {
+    //   labels: ['price'],
+    //   datasets: [
+    //     {
+    //       data: [this.props.price],
+    //       backgroundColor: gradient,
+    //       borderWidth: 1,
+    //       hoverBackgroundColor: 'rgba(120, 187, 232, 0.4)'
+    //     }
+    //   ]
+    // };
+    // let options = {
+    //   legend: {
+    //     display: false
+    //   }
+    // };
 
     return (
       <div className="price-container">
         <h1 className={`animated ${animation} price`}>{this.props.price}</h1>
         <div className="chart">
-          <Doughnut
-            className="donut"
-            data={data}
-            width={300}
-            height={300}
-            options={options}
-          />
+          <DonutChart price={this.props.price} />
         </div>
       </div>
     );
