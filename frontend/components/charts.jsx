e6a5fcdee6b1fbaa4e5d0dfeab1440443678995d
Chao Fan <fanchao05@gmail.com>
2018-05-20 15:24:26 -0400

    updated

--- frontend/components/charts.jsx
+++ frontend/components/charts.jsx
@@ -4,13 +4,13 @@ import LineChart from './line_chart';
 
 class Charts extends React.Component {
   render() {
-    // let price;
-    // if (!this.props.price) {
-    //   price = '';
-    // }
-    // let remiander = (this.props.price % 1000).toString();
-    // price = Math.floor(this.props.price / 1000);
-    // price = price > 0 ? `$${price},${remiander}` : `$${remiander}`;
+    let price;
+    console.log('lalala', this.props.price);
+    if (this.props.price) {
+      let remiander = (this.props.price % 1000).toString();
+      price = Math.floor(this.props.price / 1000).toString();
+      price = price > 0 ? `$${price},${remiander}` : `$${remiander}`;
+    }
     const animationList = [
       'bounce',
       'pulse',
@@ -33,13 +33,11 @@ class Charts extends React.Component {
       <div className="price-container">
         <div className="charts">
           <div className="donut-chart">
-            <h1 className={`animated ${animation} price`}>
-              {this.props.price}
-            </h1>
+            <h1 className={`animated ${animation} price`}>{price}</h1>
             <DonutChart price={this.props.price} />
           </div>
           <div className="line-chart">
-            <LineChart data={this.props.dataHash} />
+            <LineChart data={this.props.hash} />
           </div>
         </div>
       </div>
