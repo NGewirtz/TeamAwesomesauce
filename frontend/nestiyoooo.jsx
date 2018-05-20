9db96a4ffc8f6c7de49a966de5626a0078d54d4a
Neil Gewirtz <ngewirtz86@gmail.com>
2018-05-19 20:01:57 -0400

    delete root

--- frontend/nestiyoooo.jsx
+++ frontend/nestiyoooo.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
 import configureStore from './store/store';
-import Root from './components/root';
 
 import { fetchListings } from './actions/listings_actions';
 window.fetchListings = fetchListings;
