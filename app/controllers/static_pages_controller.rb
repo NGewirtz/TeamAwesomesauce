d3fb65700831be66df06308d967c352fde6cf4ba
Neil Gewirtz <ngewirtz86@gmail.com>
2018-05-23 09:36:54 -0400

    remove test code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,5 @@ require 'httparty'
 
 class StaticPagesController < ApplicationController
   def root
-    # auth = {:username => "7950b9f2b4f040a190896f92c9ed81c6", :password => ""}
-    # @json = HTTParty.get("https://nestiolistings.com/api/v2/listings/residential/rentals/", :basic_auth => auth).body
   end
 end
